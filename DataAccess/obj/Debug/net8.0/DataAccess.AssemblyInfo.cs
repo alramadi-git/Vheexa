4d1a19497e816e6e03cd96944db8dba28929ed4f
nawaf alramadi <alramadi.me@gmail.com>
2025-06-26 15:07:53 +0300

    2025/6/26 0:00 PM | Work - Planning

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c17978145dbecdec59c9cad269551e5c52233a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2be819c7d144c6c16dd6624ac7125e81b342c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
