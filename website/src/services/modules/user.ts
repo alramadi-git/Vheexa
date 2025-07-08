1265b0fbe51846c4d1c01cc0482ac920b5357d85
nawaf alramadi <alramadi.me@gmail.com>
2025-07-09 01:08:02 +0300

    home

---
+++ website/src/services/modules/user.ts
@@ -0,0 +1,12 @@
+type TUser = {
+  ID: string;
+  FirstName: string;
+  MiddleName: string;
+  LastName: string;
+  BirthDay: Date;
+  Email: string;
+  UpdatedAt: Date;
+  CreatedAt: Date;
+};
+
+export type { TUser };
