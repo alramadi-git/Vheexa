c4f6af42dd92161335c23a9d45659cf8c0341bb1
nawaf alramadi <alramadi.me@gmail.com>
2025-11-12 06:48:55 +0300

    ...

--- Frontend/website/src/components/locals/blocks/link.tsx
+++ Frontend/website/src/components/locals/blocks/link.tsx
@@ -5,23 +5,16 @@ import type { ComponentProps } from "react";
 import { useSearchParams } from "next/navigation";
 
 // eslint-disable-next-line no-restricted-imports
-import { Link as NextLink, usePathname } from "@/i18n/navigation";
+import { usePathname, Link as I18NLink } from "@/i18n/navigation";
 
-function Link(props: ComponentProps<typeof NextLink>) {
-  return <NextLink {...props} />;
+export function Link(props: ComponentProps<typeof I18NLink>) {
+  return <I18NLink {...props} />;
 }
 
-function LinkLocale(
-  props: Omit<ComponentProps<typeof Link>, "href">,
-) {
+export function LinkLocale(props: Omit<ComponentProps<typeof Link>, "href">) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   const href = `${pathname}${searchParams.size === 0 ? "" : "?"}${searchParams.toString()}`;
   return <Link href={href} {...props} />;
-}
-
-export {
-  Link,
-  LinkLocale,
 }
